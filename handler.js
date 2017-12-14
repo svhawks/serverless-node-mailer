2e0263462005576258bd59a51cda6f9a26bd00e7
Sadik Ay <sadikay91@gmail.com>
2017-12-14 07:16:30 +0300

    add handlebars

--- handler.js
+++ handler.js
@@ -1,11 +1,21 @@
 'use strict';
-const querystring = require('querystring')
-module.exports.helloWorld = (event, context, callback) => {
+require('handlebars');
 
+module.exports.helloWorld = (event, context, callback) => {
+  const opt = JSON.parse(event.options)
+  console.log(event.options);
+  console.log(event.content);
+  console.log(opt.email_from);
+
+  var template = Handlebars.compile(event.content);
+  var data = JSON.parse(event.options);
+  var result = template(data);
+  console.log(result);
+  
   const response = {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+      'Access-Control-Allow-Origin': '*',
     },
     body: JSON.stringify({
       message: event
