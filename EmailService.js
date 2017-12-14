e67bc0c2f2753bbbac4dc88d20dbf55cb2d79b6f
Sadik Ay <sadikay91@gmail.com>
2017-12-14 20:57:04 +0300

    add smtp email sender

---
+++ EmailService.js
@@ -0,0 +1,59 @@
+const NodeMailer = require('nodemailer')
+
+class EmailService {
+
+    constructor(smtpHost, smtpUsername, smtpPassword, smtpPort) {
+        this.smtpHost = smtpHost;
+        this.smtpUsername = smtpUsername;
+        this.smtpPassword = smtpPassword;
+        this.smtpPort = smtpPort;
+    }
+
+
+    _smtpConnect() {
+        return new Promise((resolve) => {
+            // create reusable transporter object using the default SMTP transport
+            let transporter = NodeMailer.createTransport({
+                host: this.smtpHost,
+                port: this.smtpPort,
+                secure: true,
+                auth: {
+                    user: this.smtpUsername,
+                    pass: this.smtpPassword
+                }
+            })
+            resolve(transporter)
+        })
+    }
+
+
+    sendEmail(from, to, subject, body) {
+        return this._smtpConnect()
+        .then((transporter) => {
+            return new Promise((resolve, reject) => {
+                // setup email data with unicode symbols
+                let mailOptions = {
+                    from: from, // sender address
+                    to: to, // list of receivers
+                    subject: subject, // Subject line
+                    text: body, // plain text body
+                    html: body // html body
+                }
+
+                // send mail with defined transport object
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                      console.log("ERRORRRR")
+                      console.log(error)
+                      reject(error)
+                    }
+                    else {
+                        resolve(info)
+                    }
+                })
+            })
+        })
+    }
+}
+
+module.exports = EmailService
--- handler.js
+++ handler.js
@@ -1,24 +1,48 @@
 'use strict';
-require('handlebars');
+var EmailService = require('./EmailService')
+var Handlebars = require('handlebars');
+
+function createEmailService(smtp) {
+    const emailService = new EmailService(
+        smtp.address,
+        smtp.username,
+        smtp.password,
+        smtp.port
+    )
+    return emailService
+}
 
 module.exports.helloWorld = (event, context, callback) => {
   const opt = JSON.parse(event.options)
-  console.log(event.options);
-  console.log(event.content);
-  console.log(opt.email_from);
+  // console.log(event.options);
+  // console.log(event.content);
+  // console.log(opt.email_from);
+
+  var smtp = JSON.parse(event.smtp);
 
   var template = Handlebars.compile(event.content);
-  var data = JSON.parse(event.options);
-  var result = template(data);
-  console.log(result);
-  
+  var templateResult = template(opt);
+
+  // Send Email
+  const emailService = createEmailService(smtp)
+
+  var service = emailService.sendEmail(
+    opt.email_from,
+    opt.email_to,
+    opt.subject,
+    templateResult
+  ).then((info) => {
+      callback(null, info)
+  })
+  .catch(error => {
+      callback(null, error)
+  })
+
   const response = {
     statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
+    headers: { 'Access-Control-Allow-Origin': '*' },
     body: JSON.stringify({
-      message: event
+      message: service
     }),
   };
 
