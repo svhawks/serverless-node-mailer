dbd4fa7cfd3f32532d48d404f0c5454b9b5ed26b
Sadik Ay <sadikay91@gmail.com>
2017-12-14 06:22:16 +0300

    fix payload

--- handler.js
+++ handler.js
@@ -1,14 +1,6 @@
 'use strict';
 const querystring = require('querystring')
 module.exports.helloWorld = (event, context, callback) => {
-  // send json as row content
-  // const body = JSON.parse(event.body);
-  // console.log(body);
-  console.log("============================================================================================================>");
-  let data = Object.assign({}, querystring.parse(event.body), event.pathParameters, event.queryStringParameters)
-  console.log(data['from_email']);
-  // console.log(event.body.from_email);
-
 
   const response = {
     statusCode: 200,
@@ -16,7 +8,7 @@ module.exports.helloWorld = (event, context, callback) => {
       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
     },
     body: JSON.stringify({
-      message: event.body
+      message: event
     }),
   };
 
